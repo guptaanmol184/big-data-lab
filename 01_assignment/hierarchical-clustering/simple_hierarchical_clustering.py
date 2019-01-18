1ba1e2d42296e11a0e1291aba11a1b9343946ec1
Anmol Gupta <guptaanmol184@gmail.com>
2019-01-18 20:40:47 +0530

    add to display functionality to simple script

--- 01_assignment/hierarchical-clustering/simple_hierarchical_clustering.py
+++ 01_assignment/hierarchical-clustering/simple_hierarchical_clustering.py
@@ -16,6 +16,8 @@
 
 import sys
 import numpy as np
+from scipy.cluster import hierarchy
+import matplotlib.pyplot as plt
 
 def hierarchical_clustering(input_file, linkage='single'):
     # load data from file
@@ -65,6 +67,38 @@ def hierarchical_clustering(input_file, linkage='single'):
 
     return dendrogram
 
+# converts a dendrogram in my format to matplotlib format, so that we can plot it
+# give me that eye candy :)
+def convert_dendrogram_to_matlab(dendrogram):
+    items_count = len(dendrogram) + 1
+    cluster_count = {}
+    for i in range(items_count):
+        cluster_count[i] = 1
+
+    mapping_dict = {}
+    z = np.ones((items_count-1)*4).reshape(items_count-1, 4)
+    ctr = items_count-1 # initialize ctr to label of last original cluster
+    for index, val in enumerate(dendrogram):
+
+        # update cluster count of combined cluster
+        cluster_count[val[3]] = cluster_count[val[0]] + cluster_count[val[1]]
+        z[index, 0] = val[0]
+        z[index, 1] = val[1]
+        z[index, 2] = val[2]
+        z[index, 3] = cluster_count[val[3]]
+
+        # update rows using mapping dict
+        if val[0] in mapping_dict:
+            z[index, 0] = mapping_dict[val[0]]
+        if val[1] in mapping_dict:
+            z[index, 1] = mapping_dict[val[1]]
+
+        ctr += 1 # increment count
+        # update table
+        mapping_dict[val[3]] = ctr
+
+    return z
+
 def main():
     if len(sys.argv) != 2:
         sys.exit('Usage: {} <input_file>'.format(sys.argv[0]))
@@ -72,5 +106,15 @@ def main():
         dendrogram = hierarchical_clustering(sys.argv[1])
         print('Stepwise Dendrogram:')
         print(dendrogram)
+
+        z = convert_dendrogram_to_matlab(dendrogram)
+        print('matplotlib format dendrogram')
+        print(z)
+
+        # plot figure in using matplotlib
+        plt.figure()
+        hierarchy.dendrogram(z)
+        plt.show()
+
 
 main()
